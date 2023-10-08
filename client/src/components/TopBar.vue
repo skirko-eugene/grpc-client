036643306b1256c5459dc420411381716696df4a
Evgen Skirko <skirko.eugene@gmail.com>
2023-10-08 23:26:48 +0300

    feat: переделал поле ввода API

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="HostInput">
-    <input type="text" v-model="val"/>
+  <div
+    class="hostInput"
+    :class="computedClass"
+  >
+    <label for="hostInput">API</label>
+    <div class="hostInput__list"><IconList /></div>
+    <input type="text" v-model="val" id="hostInput" @blur="focusOut" @focus="focusIn" />
     <span v-if="isLoading">Загркзка</span>
     <span v-if="fetchError">{{fetchError.message}}</span>
+    <div class="hostInput__enter"><IconEnter /></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, } from 'vue';
+import { computed, ref } from 'vue';
+import IconEnter from '../icons/IconEnter.vue';
+import IconList from '../icons/IconList.vue';
 
 interface Props {
   host: string;
@@ -28,37 +36,104 @@ const val = computed({
   set(value: string) {
     emit('host', value)
   }
-})
+});
 
+const focusFlag = ref(false);
 
+const focusIn = () => {
+  focusFlag.value = true;
+}
+const focusOut = () => {
+  focusFlag.value = false;
+}
+
+const computedClass = computed(() => {
+  return {
+    'hostInput--focus': focusFlag.value,
+  }
+});
 </script>
 
-<style scoped>
-.HostInput {
-  width: 100%;
-  background: rgb(var(--color-2));
+<style lang="postcss" scoped>
+.hostInput {
+  max-width: 300px;
+  border-radius: var(--radius-s);
+  border: 1px solid rgb(var(--gray03));
+  background-color: rgb(var(--gray02));
   display: flex;
   align-items: center;
-  grid-area: top;
+  margin: 40px;
+  position: relative;
+  & label {
+    display: block;
+    position: absolute;
+    left: 10px;
+    top: -20px;
+    color: rgb(var(--gray06));
+    font-size: 12px;
+    line-height: 1;
+  }
+}
+
+.hostInput--focus {
+  border-color: rgb(var(--gray04))
 }
 
 input[type="text"] {
-  flex-grow: 0.4;
-  border: none;
-  /* border: 1px solid red; */
-  border-radius: 3px;
+  width: 100%;
   box-sizing: border-box;
-  margin: 6px 8px;
-  padding: 4px 20px;
-  color: rgba(var(--color-font), .6);
-
   box-shadow: none;
-
-  font: inherit;
-  font-size: 15px;
+  margin: 0;
+  outline: 0;
+  border: 0;
+  padding: 5px 10px 6px;
+  background-color: transparent;
+  color: rgb(var(--gray07));
+  font-family: var(--defaultFont);
+  font-size: 13px;
+  line-height: 1;
+  letter-spacing: 0.3px;
 }
-input[type="text"]:focus {
-  color: rgb(var(--color-font));
-}
 
+.hostInput__list {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  border-right: 1px solid rgb(var(--gray03));
+  width: 36px;
+  height: 36px;
+  overflow: hidden;
+  cursor: pointer;
+  & svg {
+    width: 16px;
+    height: 16px;
+    --color: rgb(var(--gray05));
+  }
+  &:hover {
+    & svg {
+      --color: rgb(var(--gray07));
+    }
+  }
+}
+.hostInput__enter {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  width: 36px;
+  height: 36px;
+  overflow: hidden;
+  cursor: pointer;
+  & svg {
+    width: 16px;
+    height: 16px;
+    --color: rgb(var(--gray05));
+  }
+  &:hover {
+    & svg {
+      --color: rgb(var(--gray07));
+    }
+  }
+}
 </style>
---
+++ client/src/icons/IconEnter.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6.00008 6.66666L2.66675 9.99999L6.00008 13.3333" stroke="var(--color, currentColor)" stroke-width="1.5" stroke-linecap="round"
+      stroke-linejoin="round" />
+    <path
+      d="M13.3334 2.66666V7.33332C13.3334 8.04057 13.0525 8.71884 12.5524 9.21894C12.0523 9.71904 11.374 9.99999 10.6667 9.99999H2.66675"
+      stroke="var(--color, currentColor)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+</svg></template>
